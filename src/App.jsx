b56d7a7ffdc3a2671241974ba2add6e257099151
Yaren Can <yarencanz@outlook.com>
2023-11-07 02:36:37 +0300

    Completed Navbar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Navbar from "./components/Navbar.jsx"
+import Home from './pages/Home'
+import Categories from './pages/Categories'
+import Popular from './pages/Popular'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
  
   return (
    <div>
      <Navbar/>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/popular" element={<Popular/>}/>
+      <Route path="/categories" element={<Categories/>}/>
+     </Routes>
    </div>
      
     
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,51 @@
-import '../App.css'
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css'
 import CSEFLIX from '../assets/CSEFLIX-logo.svg'
 import Vector from '../assets/Vector.svg'
+import { useState } from 'react';
 
 function Navbar(){
+ const [searchText, setSearchText] = useState("");
+ const [changeColorPopular, setChangeColorPopular] = useState(false);
+ const [changeColorCategories, setChangeColorCategories] = useState(false);
+ let location = useLocation();
+ console.log(location.pathname);
+ 
+ function handleClick(){
+    
+    /*if(location.pathname === "/popular"){
+    setChangeColorPopular(true);
+    console.log(changeColorPopular);
+    }else {
+        setChangeColorPopular(false);
+    }
+    if(location.pathname === "/categories"){
+        setChangeColorCategories(true);
+    }else{
+        setChangeColorCategories(false);
+        
+    }*/
+ }
+   
+
     return(
        <nav className='nav-container'>
-           <div >
-            <img className='logo' src={CSEFLIX} />
-           </div>
+           <Link to="/" >
+            <img className='logo' src={CSEFLIX}/>
+           </Link>
            <div className='titles'>
-            <h1>Popular</h1>
-            <h1>Categories</h1>
+            <Link to="/popular"  className={location.pathname === "/popular" ? "title-clicked" : "title-notClick"}  >Popular</Link>
+            <Link to="/categories" className={location.pathname === "/categories" ? "title-clicked" : "title-notClick"} >Categories</Link>
            </div>
            <div className='search-container'>
            <input 
            className='search-box'
             type="text"
-            //value={searchText}
-            //onChange={handleSearchChange}
+            value={searchText}
+            onChange={(event) =>{setSearchText(event.target.value)}}
             placeholder="Search..." />
-           <img className='Vector' src={Vector}/>
+           <img className='Vector' src={Vector} /*onClick={searchFunction}*//>
+           
            </div>
       </nav>
     )
