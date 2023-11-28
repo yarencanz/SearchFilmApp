683f697e50fd8598afc9c0c727a965c14e09f004
Yaren Can <yarencanz@outlook.com>
2023-11-28 14:51:25 +0300

    Fetch request to database.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, json, useLocation } from 'react-router-dom';
 import './Navbar.css'
 import CSEFLIX from '../assets/CSEFLIX-logo.svg'
 import Vector from '../assets/Vector.svg'
@@ -6,28 +6,21 @@ import { useState } from 'react';
 
 function Navbar(){
  const [searchText, setSearchText] = useState("");
- const [changeColorPopular, setChangeColorPopular] = useState(false);
- const [changeColorCategories, setChangeColorCategories] = useState(false);
  let location = useLocation();
- console.log(location.pathname);
  
- function handleClick(){
-    
-    /*if(location.pathname === "/popular"){
-    setChangeColorPopular(true);
-    console.log(changeColorPopular);
-    }else {
-        setChangeColorPopular(false);
-    }
-    if(location.pathname === "/categories"){
-        setChangeColorCategories(true);
-    }else{
-        setChangeColorCategories(false);
-        
-    }*/
+ const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmMzNmQzYmQ1OWViYTQ5NGVjZTY3ZjRkYTY3MDc2NCIsInN1YiI6IjY1NjM0NGNlNzA2ZTU2MDBjNGJiOTU2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F2uE30anBqd6H4A6dYGJ7qguv6oHPNJOkmpbPhDfKwM'
+  }
+};
+ const getMovieOrTv= () => {
+  fetch(`https://api.themoviedb.org/3/search/multi?query=${searchText}&include_adult=false&language=en-US&page=1`,options)
+  .then(response => response.json())
+  .then(response => setSearchText(""))
  }
-   
-
+ 
     return(
        <nav className='nav-container'>
            <Link to="/" >
@@ -37,14 +30,19 @@ function Navbar(){
             <Link to="/popular"  className={location.pathname === "/popular" ? "title-clicked" : "title-notClick"}  >Popular</Link>
             <Link to="/categories" className={location.pathname === "/categories" ? "title-clicked" : "title-notClick"} >Categories</Link>
            </div>
-           <div className='search-container'>
+           <div className='search-container' >
            <input 
            className='search-box'
             type="text"
             value={searchText}
             onChange={(event) =>{setSearchText(event.target.value)}}
-            placeholder="Search..." />
-           <img className='Vector' src={Vector} /*onClick={searchFunction}*//>
+            placeholder="Search..." 
+            onKeyDown={(e) => {
+              if(e.code == "Enter"){
+                  getMovieOrTv();
+              }
+          }}/>
+           <img className='Vector' src={Vector}  onClick={() => {getMovieOrTv()}}/>
            
            </div>
       </nav>
