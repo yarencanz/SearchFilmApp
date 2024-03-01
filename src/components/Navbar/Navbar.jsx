7e60ab15099e75016ff162714a2b6682e1e2ee00
Yaren Can <yarencanz@outlook.com>
2024-03-01 18:44:51 +0300

    Review navbar page.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,54 +1,89 @@
-import { Link, json, useLocation } from 'react-router-dom';
+import { Link, json, useLocation } from 'react-router-dom'
 import './Navbar.css'
 import CSEFLIX from '../../assets/CSEFLIX-logo.svg'
 import Vector from '../../assets/Vector.svg'
-import { useState } from 'react';
+import { useState } from 'react'
 import '../../pages/Popular/Popular.jsx'
 
-function Navbar(){
- const [searchText, setSearchText] = useState("");
- let location = useLocation();
- 
- const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmMzNmQzYmQ1OWViYTQ5NGVjZTY3ZjRkYTY3MDc2NCIsInN1YiI6IjY1NjM0NGNlNzA2ZTU2MDBjNGJiOTU2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F2uE30anBqd6H4A6dYGJ7qguv6oHPNJOkmpbPhDfKwM'
-  }
-};
- const getMovieOrTv= () => {
-  fetch(`https://api.themoviedb.org/3/search/multi?query=${searchText}&include_adult=false&language=en-US&page=1`,options)
-  .then(response => response.json())
-  .then(response => setSearchText(""))
- }
+function Navbar() {
+    const [searchText, setSearchText] = useState('')
+    let location = useLocation()
 
- 
-    return(
-       <nav className='nav-container'>
-           <Link to="/" >
-            <img className='logo' src={CSEFLIX}/>
-           </Link>
-           <div className='titles'>
-            <Link to="/popular"  className={location.pathname === "/popular" ? "title-clicked" : "title-notClick"} >Popular</Link>
-            <Link to="/categories" className={location.pathname === "/categories" ? "title-clicked" : "title-notClick"} >Categories</Link>
-           </div>
-           <div className='search-container' >
-           <input 
-           className='search-box'
-            type="text"
-            value={searchText}
-            onChange={(event) =>{setSearchText(event.target.value)}}
-            placeholder="Search..." 
-            onKeyDown={(e) => {
-              if(e.code == "Enter"){
-                  getMovieOrTv();
-              }
-          }}/>
-           <img className='Vector' src={Vector}  onClick={() => {getMovieOrTv()}}/>
-           
-           </div>
-      </nav>
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmMzNmQzYmQ1OWViYTQ5NGVjZTY3ZjRkYTY3MDc2NCIsInN1YiI6IjY1NjM0NGNlNzA2ZTU2MDBjNGJiOTU2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F2uE30anBqd6H4A6dYGJ7qguv6oHPNJOkmpbPhDfKwM',
+        },
+    }
+    const getMovieOrTv = () => {
+        useEffect(() => {
+            fetch(
+                `https://api.themoviedb.org/3/search/multi?query=${searchText}&include_adult=false&language=en-US&page=1`,
+                options
+            )
+                .then((response) => response.json())
+                .then((response) => setSearchText(() => response?.results))
+                .then((response) => setSearchText((response) => ''))
+                .catch((err) => console.error(err))
+        }, [])
+    }
+    return (
+        <nav className="nav-container">
+            <Link to="/">
+                <img className="logo" src={CSEFLIX} />
+            </Link>
+            <div className="titles">
+                <Link
+                    to="/popular"
+                    className={
+                        location.pathname === '/popular'
+                            ? 'title-clicked'
+                            : 'title-notClick'
+                    }
+                >
+                    {' '}
+                    Popular
+                </Link>
+                <Link
+                    to="/categories"
+                    className={
+                        location.pathname === '/categories'
+                            ? 'title-clicked'
+                            : 'title-notClick'
+                    }
+                >
+                    Categories
+                </Link>
+            </div>
+            <div className="search-container">
+                <input
+                    className="search-box"
+                    type="text"
+                    value={searchText}
+                    onChange={(event) => {
+                        setSearchText(event.target.value)
+                    }}
+                    placeholder="Search..."
+                    onKeyDown={(e) => {
+                        if (e.code == 'Enter') {
+                            getMovieOrTv()
+                        }
+                    }}
+                />
+                <Link to={'/moviePage'}>
+                    <img
+                        className="Vector"
+                        src={Vector}
+                        onClick={() => {
+                            getMovieOrTv()
+                        }}
+                    />
+                </Link>
+            </div>
+        </nav>
     )
 }
 
-export default Navbar;
+export default Navbar
